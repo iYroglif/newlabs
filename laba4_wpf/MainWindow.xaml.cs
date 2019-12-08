f09b1afde3f5d03fc5990822db3f51ab50191f00
iYroglif <webman.com@yandex.ru>
2019-12-08 19:43:53 +0300

    labs 4_code + 5_lib

---
+++ laba4_wpf/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+
+namespace laba4_wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        readonly List<string> words = new List<string>();
+
+        private void ButtonLoadFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog
+            {
+                Filter = "Текстовые Файлы|*.txt"
+            };
+            Nullable<bool> result = fd.ShowDialog();
+            if (result == true)
+            {
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+                string text = File.ReadAllText(fd.FileName);
+                char[] separators = new char[] { ' ', '.', ',', '?', '!', '\"', '<', '>', '/', '\t', '\n' };
+                string[] textArray = text.Split(separators);
+                foreach (string strTemp in textArray)
+                {
+                    string str = strTemp.Trim();
+                    if (str != "")
+                    {
+                        if (!words.Contains(str)) words.Add(str);
+                    }
+                }
+                timer.Stop();
+                this.LabelTimer.Content = timer.Elapsed.ToString();
+                this.TextBoxCountWords.Text = words.Count.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл");
+            }
+        }
+
+        private void ButtonSearchWord_Click(object sender, RoutedEventArgs e)
+        {
+            string wordSearch = this.TextBoxSearchWord.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(wordSearch) && words.Count > 0)
+            {
+                string wordUpper = wordSearch.ToUpper();
+                List<string> tempList = new List<string>();
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+                foreach (string str in words)
+                {
+                    if (str.ToUpper().Contains(wordUpper))
+                    {
+                        tempList.Add(str);
+                    }
+                }
+                timer.Stop();
+                this.LabelTimeSearch.Content = timer.Elapsed.ToString();
+                this.ListBoxResult.ItemsSource=tempList;
+            }
+            else
+            {
+                MessageBox.Show("Выберите файл и введите слово для поиска");
+            }
+        }
+    }
+}
---
+++ laba5_lib/LevenshteinDistance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba5_lib
+{
+    public static class LevenshteinDistance
+    {
+        public static int Distance(string strFirst, string strSecond)
+        {
+            if ((strFirst == null) || (strSecond == null)) return -1;
+            int str1Len = strFirst.Length;
+            int str2Len = strSecond.Length;
+            if ((str1Len == 0) && (str2Len == 0)) return 0;
+            if (str1Len == 0) return str2Len;
+            if (str2Len == 0) return str1Len;
+            string str1 = strFirst.ToUpper();
+            string str2 = strSecond.ToUpper();
+            if (str1 == str2) return 0;
+            int[,] matrix = new int[str1Len + 1, str2Len + 1];
+            for (int i = 0; i <= str1Len; i++) matrix[i, 0] = i;
+            for (int j = 0; j <= str2Len; j++) matrix[0, j] = j;
+            for (int i = 1; i <= str1Len; i++)
+            {
+                for (int j = 1; j <= str2Len; j++)
+                {
+                    int cost = ((str1.Substring(i - 1, 1) == str2.Substring(j - 1, 1)) ? 0 : 1);
+                    int ins = matrix[i, j - 1] + 1;
+                    int del = matrix[i - 1, j] + 1;
+                    int subst = matrix[i - 1, j - 1] + cost;
+                    matrix[i, j] = Math.Min(Math.Min(ins, del), subst);
+                    if ((i > 1) && (j > 1) &&
+                        (str1.Substring(i - 1, 1) == str2.Substring(j - 2, 1)) &&
+                        (str1.Substring(i - 2, 1) == str2.Substring(j - 1, 1)))
+                    {
+                        matrix[i, j] = Math.Min(matrix[i, j], matrix[i - 2, j - 2] + cost);
+                    }
+                }
+            }
+            return matrix[str1Len, str2Len];
+        }
+    }
+}
